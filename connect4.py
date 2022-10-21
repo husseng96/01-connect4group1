e1c365ebc7559f4b44a8a6a6dc903e20985cae71
jolin520 <64655186+Jolin520@users.noreply.github.com>
2022-10-20 22:24:03 -0400

    checking that colum has an empty slot

--- connect4.py
+++ connect4.py
@@ -4,26 +4,32 @@ import numpy as np
 # def main():
 # board=create_board()
 # print(board)
-
+ROW_COUNT = 6
+COLUM_COUNT =7
 def create_board():
     board = np.zeros((6, 7))
     return board
 
 
-def drop_piece():
-    pass
+def drop_piece(board ,row, col, piece):
+    board[row][col] = piece
 
 
 def is_vaild_loacation(board, col):
-    pass
+    return board[5][col] == 0
 
 
-def get_next_open_row():
-    pass
+def get_next_open_row(board, col ):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+def print_board(board):
+    print(np.flip(board,0))
 
 
 board = create_board()
-print(board)
+print_board(board)
 game_over = False
 turn = 0
 
@@ -34,10 +40,21 @@ while not game_over:
 
         col = int(input("Player 1 make your selection (0-6):"))
 
+        if is_vaild_loacation(board,col):
+            row = get_next_open_row(board,col)
+            drop_piece(board,row,col,1)
+
+
+
         # ask for Player 2 input
 
     else:
         col = int(input("Player 2 make your selection (0-6):"))
 
+        if is_vaild_loacation(board,col):
+            row = get_next_open_row(board,col)
+            drop_piece(board,row,col,2)
+
+    print_board(board)
     turn += 1
     turn = turn % 2
