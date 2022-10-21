f2bf02f1cfaef47bb7f462e8e3ab707d460b9949
jolin520 <64655186+Jolin520@users.noreply.github.com>
2022-10-20 22:04:20 -0400

    some changes for player 1 and 2

--- connect4.py
+++ connect4.py
@@ -32,12 +32,12 @@ while not game_over:
 
     if turn == 0:
 
-        col = int(input("Player 1 make your seclection (0-6);"))
+        col = int(input("Player 1 make your selection (0-6):"))
 
-        ## ask for Player 2 input
+        # ask for Player 2 input
 
     else:
-        col = int(input("Player 2 make your seclection (0-6);"))
+        col = int(input("Player 2 make your selection (0-6):"))
 
     turn += 1
     turn = turn % 2
