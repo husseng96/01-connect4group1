0dd3a0aaace56a8ba775006c06b10ce57c109bf2
jolin520 <64655186+Jolin520@users.noreply.github.com>
2022-10-20 21:20:36 -0400

    ask for player 1 and  2 to input

--- connect4.py
+++ connect4.py
@@ -1,11 +1,43 @@
 import numpy as np
 
-def main():
-    board=create_board()
-    print(board)
-    
+
+# def main():
+# board=create_board()
+# print(board)
+
 def create_board():
-    board=np.zeros((6,7))
+    board = np.zeros((6, 7))
     return board
 
-main()
+
+def drop_piece():
+    pass
+
+
+def is_vaild_loacation(board, col):
+    pass
+
+
+def get_next_open_row():
+    pass
+
+
+board = create_board()
+print(board)
+game_over = False
+turn = 0
+
+while not game_over:
+    # ask for Player 1  input
+
+    if turn == 0:
+
+        col = int(input("Player 1 make your seclection (0-6);"))
+
+        ## ask for Player 2 input
+
+    else:
+        col = int(input("Player 2 make your seclection (0-6);"))
+
+    turn += 1
+    turn = turn % 2
