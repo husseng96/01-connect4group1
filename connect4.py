d5f06d81297dcd15498e49816ed7e73ebb0662f8
tuj04825 <tuj04825@temple.edu>
2022-10-25 17:54:26 -0400

    All buttons work
    the buttons work. have to work on actual gameplay and also rules page

--- connect4.py
+++ connect4.py
@@ -1,63 +1,70 @@
-
 import numpy as np
 import pygame
 import sys
 
 pygame.init()
 
-#color blue
+#colors
 BLUE=(0,0,200)
-BLACK = (0,0,0)
+LIGHT_BLUE=(0,102,255)
+LIGHT_WHITE = (170, 170, 170)
+DARK_WHITE = (100, 100, 100)
+
 PIXELS_FOR_SQUARES=100
 RADIUS = int(PIXELS_FOR_SQUARES/2 - 5)
 
-#chips red - 1st, yellow - 2nd
-
-
-game_over=True
-def main():
-    #create board
-    board=board_gen()
-
-    #print(board)
-    #pygame GUI
-    pygame.init()
-    gui_size=(900,900)
-    #gui=pygame.display.set_mode(gui_size)
-    screen=pygame.display.set_mode(gui_size)
-    pygame.display.set_caption('Connect 4')
-
-### start menu
-res = (720, 720)
-
+#Screen
+res = (1280, 720)
 screen = pygame.display.set_mode(res)
-
-white = (255, 255, 255)
-
-white_light = (170, 170, 170)
-
-white_dark = (100, 100, 100)
-
 width = screen.get_width()
-
 height = screen.get_height()
 
-gamefont = pygame.font.SysFont('Ariel', 35)
+BG = pygame.image.load("Dots_BG.jpeg")
+BG = pygame.transform.scale(BG, res)
 
-singeoption = gamefont.render("Single Player", True, white)
+#Text
+header_font = pygame.font.SysFont('markerfelt', 120)
+game_font = pygame.font.SysFont('arial', 35)
 
-multioption = gamefont.render("Multiplayer", True, white)
+title = header_font.render('Connect  F  o  u  r', True, "white")
+#Button
+single_option = game_font.render("Single Player", True, "white")
+single_hover = game_font.render("Single Player", True, DARK_WHITE)
+multi_option = game_font.render("Multiplayer", True, "white")
+multi_hover = game_font.render("Multiplayer", True, DARK_WHITE)
+rules_option = game_font.render("Rules", True, "white")
+rules_hover = game_font.render("Rules", True, DARK_WHITE)
+quit_option = game_font.render('Quit', True, "black")
 
-rulesoption = gamefont.render("Rules", True, white)
 
-quitoption = gamefont.render('Quit', True, white)
+def single():
+    while True:
+        screen.fill(LIGHT_BLUE)
+
+        board=board_gen()
+        board_gen_gui(screen)
+
+        pygame.display.update()
+
+def multi():
+    while True:
+        screen.fill(LIGHT_BLUE)
+
+        board=board_gen()
+        board_gen_gui(screen)
+
+        pygame.display.update()
+
+def rules():
+    while True:
+        screen.fill("white")
+
+        pygame.display.update()
 
 def board_gen_gui(screen):
     for c in range(7):
         for r in range(6):
-            pygame.draw.rect(screen, BLUE, (c*PIXELS_FOR_SQUARES, r*PIXELS_FOR_SQUARES, PIXELS_FOR_SQUARES,
-                                            PIXELS_FOR_SQUARES))
-            pygame.draw.circle(screen, BLACK, (int(c*PIXELS_FOR_SQUARES + PIXELS_FOR_SQUARES/2),
+            pygame.draw.circle(screen, "white", (int(c*PIXELS_FOR_SQUARES + PIXELS_FOR_SQUARES/2),
                                int(r*PIXELS_FOR_SQUARES + PIXELS_FOR_SQUARES + PIXELS_FOR_SQUARES/2)),
                                RADIUS)
 
@@ -67,45 +74,73 @@ def board_gen():
     board=np.zeros((6,7))
     return board
 
-while True:
-    for choice in pygame.event.get():
-        if choice.type == pygame.QUIT:
-            pygame.quit()
-
-        if choice.type == pygame.MOUSEBUTTONDOWN:
-            if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] < + height / 2 + 40:
+def main_menu():
+    while True:
+        for choice in pygame.event.get():
+            if choice.type == pygame.QUIT:
                 pygame.quit()
 
-    screen.fill((60, 25, 60))
+            if choice.type == pygame.MOUSEBUTTONDOWN:
+                if single_x <= mouse[0] <= single_x + 210  and single_y <= mouse[1] <= single_y + 40:
+                    single()
+                elif multi_x <= mouse[0] <= multi_x + 170 and multi_y <= mouse[1] <= multi_y + 40:
+                    multi()
+                elif rules_x <= mouse[0] <= rules_x + 95 and rules_y <= mouse[1] <= rules_y + 40:
+                    rules()
+                elif quit_x-10 <= mouse[0] <= quit_x+76 and quit_y <= mouse[1] <= quit_y + 40:
+                    pygame.quit()
 
-    mouse = pygame.mouse.get_pos()
+        screen.blit(BG, (0, 0))
+        mouse = pygame.mouse.get_pos()
 
-    if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
-        pygame.draw.rect(screen, white_light, [width / 2 - 100, height / 2, 140, 40])
+        #if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
+        #    pygame.draw.rect(screen, LIGHT_WHITE, [width / 2 - 100, height / 2, 140, 40])
+        #else:
+        #    pygame.draw.rect(screen, DARK_WHITE, [width / 2 - 100, height / 2, 140, 40])
 
-    else:
-        pygame.draw.rect(screen, white_dark, [width / 2 - 100, height / 2, 140, 40])
+        #Title
+        pygame.draw.circle(screen, "red", (692, 80), 58)
+        pygame.draw.circle(screen, "yellow", (805, 90), 48)
+        pygame.draw.circle(screen, "red", (910, 90), 48)
+        pygame.draw.circle(screen, "yellow", (1015, 90), 48)
 
-    ### draws the buttons
-    screen.blit(quitoption, (width / 2 - 50, height / 2))
+        screen.blit(title,(220,20))
 
-    screen.blit(singeoption,(width / 2 - 100, height / 2 - 50))
+        #Single button
+        single_x = width/2 - 100
+        single_y = height/2 - 90
+        screen.blit(single_option,(single_x, single_y))
+        if single_x <= mouse[0] <= single_x + 210 and single_y <= mouse[1] <= single_y + 40:
+            screen.blit(single_hover, (single_x, single_y))
 
-    screen.blit(multioption,(width / 2 - 90, height / 2 - 100))
+        #Multi button
+        multi_x = width/2 - 80
+        multi_y = height/2 - 40
+        screen.blit(multi_option,(multi_x, multi_y))
+        if multi_x <= mouse[0] <= multi_x + 170 and multi_y <= mouse[1] <= multi_y + 40:
+            screen.blit(multi_hover, (multi_x, multi_y))
 
-    screen.blit(rulesoption,(width / 2 - 60 , height / 2 - 150))
+        #Rules Button
+        rules_x = width/2 - 40
+        rules_y = height/2 + 10
+        screen.blit(rules_option, (rules_x, rules_y))
+        if rules_x <= mouse[0] <= rules_x + 95 and rules_y <= mouse[1] <= rules_y + 40:
+            screen.blit(rules_hover, (rules_x, rules_y))
+
+        #Quit Button
+        quit_x = width/2 - 30
+        quit_y = height/2 + 70
+        pygame.draw.rect(screen, LIGHT_WHITE, [quit_x-10, quit_y, 86, 40])
+        if quit_x-10 <= mouse[0] <= quit_x+76 and quit_y <= mouse[1] <= quit_y + 40:
+            pygame.draw.rect(screen, DARK_WHITE, [quit_x - 10, quit_y, 86, 40])
+        screen.blit(quit_option, (quit_x, quit_y))
 
 
 
-    pygame.display.update()
-    board_gen_gui(screen)
-    pygame.display.update()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
+        pygame.display.update()
 
-
-
-
-main()
+main_menu()
